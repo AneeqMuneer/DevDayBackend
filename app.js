f74d6a80dd5b79c2d66b7cedec6540dc3fb15ac9
Aneeq Muneer <aneeq.muneer03@gmail.com>
2025-02-10 23:37:28 +0500

    resolved cors issue

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const middleware = require("./Middleware/error");
+const cors = require("cors");
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.options('*', cors());
 
 app.use(express.json());
 
