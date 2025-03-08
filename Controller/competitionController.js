12b30773b29d84b9604eea1f53d975298090bfa8
Shaheer Luqman <k214655@nu.edu.pk>
2025-03-08 15:27:33 +0500

    Order Fixed

--- Controller/competitionController.js
+++ Controller/competitionController.js
@@ -2,6 +2,7 @@ const catchAsyncError = require("../Middleware/asyncError");
 const { Op } = require("sequelize");
 
 const competitionModel = require("../Model/competitionModel");
+const sequelize = competitionModel.sequelize;
 const ErrorHandler = require("../Utils/errorHandler");
 
 exports.getAllCompetitions = catchAsyncError(async (req, res, next) => {
@@ -16,7 +17,19 @@ exports.getAllCompetitions = catchAsyncError(async (req, res, next) => {
     }
 
     const competitions = await competitionModel.findAll({
-        where: whereClause
+        where: whereClause,
+        order: [
+            [
+                sequelize.literal(`CASE 
+                    WHEN "Competition_Type" = 'CS' THEN 1 
+                    WHEN "Competition_Type" = 'EE' THEN 2 
+                    WHEN "Competition_Type" = 'GC' THEN 3 
+                    WHEN "Competition_Type" = 'ES' THEN 4 
+                    ELSE 5 
+                END`),
+                'ASC'
+            ]
+        ]
     });
 
     res.status(200).json({
